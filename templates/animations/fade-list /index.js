d69e286695e8dcced62562735597c835540d987b
Gabriel Davi <gdavi.gd@gmail.com>
2020-05-11 20:40:16 -0300

    Trying to make a search input to hideable

--- templates/animations/fade-list /index.js
+++ templates/animations/fade-list /index.js
@@ -2,24 +2,65 @@ import React, { useState, useRef } from "react";
 import imageFactory from "../../utils/factory/imageFactory";
 import ListItem from "./ListItem";
 
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  PanResponder,
+  Animated,
+  ScrollView,
+} from "react-native";
 import styles from "./style";
+
+const dy = new Animated.Value(100);
 
 export default function List() {
   const images = useRef(imageFactory(10)).current;
+  const currentOffset = useRef(0);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Animation Paradise!</Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Dumb Button</Text>
-      </TouchableOpacity>
+      <Animated.Text
+        style={[
+          styles.title,
+          {
+            height: dy.interpolate({
+              inputRange: [0, 0, 100],
+              outputRange: [0, 0, 50],
+              extrapolate: "clamp",
+            }),
+          },
+        ]}
+      >
+        Animation Paradise!
+      </Animated.Text>
+      <Animated.View>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          scrollEnabled={true}
+          onScroll={({ nativeEvent }) => {
+            console.log(
+              Math.abs(currentOffset.current - nativeEvent.contentOffset.y)
+            );
+            // dy.setValue(
+            //   Math.abs(currentOffset.current - nativeEvent.contentOffset.y)
+            // );
 
-      <FlatList
-        data={images}
-        keyExtractor={(image) => image.text}
-        renderItem={({ item: image }) => <ListItem image={image} />}
-      />
+            // if (event.nativeEvent.velocity.y < 0) {
+            //   textInputHeight.setValue(-event.nativeEvent.contentOffset.y);
+            // } else {
+            //   textInputHeight.setValue(-event.nativeEvent.contentOffset.y);
+            // }
+          }}
+          onScrollEndDrag={({ nativeEvent }) =>
+            (currentOffset.current = nativeEvent.contentOffset.y)
+          }
+        >
+          {images.map((image) => (
+            <ListItem key={image.text} image={image} />
+          ))}
+        </ScrollView>
+      </Animated.View>
     </View>
   );
 }
