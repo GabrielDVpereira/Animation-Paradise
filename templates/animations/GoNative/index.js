86d817ac75001b8a757285bc579531d69e08c405
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-12 17:30:31 -0300

    Adding static layout

--- templates/animations/GoNative/index.js
+++ templates/animations/GoNative/index.js
@@ -1,55 +1,83 @@
 import React, { useState } from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableWithoutFeedback,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import list from "./list";
+import Card from "./card";
 
 export default function GoNative() {
+  const [selectedCard, setSelectedCard] = useState(null);
+  console.log(selectedCard);
   return (
     <View style={{ flex: 1 }}>
-      <View
-        style={{
-          width: "100%",
-          height: "30%",
-          marginTop: 0,
-          backgroundColor: "#38b0de",
-          justifyContent: "flex-end",
-        }}
-      >
-        <Text
-          style={{
-            fontFamily: "montserrat-bold",
-            fontSize: 26,
-            margin: 20,
-            color: "#fff",
-          }}
-        >
-          GoNative
-        </Text>
-      </View>
-      <ScrollView>
-        {list.map((card) => (
-          <View key={card.name} style={{ margin: 20 }}>
-            <Image
-              source={card.image}
-              style={{ width: "100%", height: 200 }}
-              resizeMode="cover"
-            />
-            <View
+      {selectedCard ? (
+        <View style={{ width: "100%", height: "30%" }}>
+          <ImageBackground
+            resizeMode="cover"
+            source={selectedCard.image}
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "#38b0de",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Text
               style={{
-                backgroundColor: card.color,
-                padding: 20,
-                flexDirection: "row",
-                justifyContent: "space-between",
+                fontFamily: "montserrat-bold",
+                fontSize: 26,
+                margin: 20,
+                color: "#fff",
               }}
             >
-              <View style={{ width: "60%" }}>
-                <Text>{card.name}</Text>
-                <Text>{card.description}</Text>
-              </View>
-              <Text>{card.likes}</Text>
-            </View>
-          </View>
-        ))}
-      </ScrollView>
+              {selectedCard.name}
+            </Text>
+          </ImageBackground>
+        </View>
+      ) : (
+        <View
+          style={{
+            width: "100%",
+            height: "30%",
+            marginTop: 0,
+            backgroundColor: "#38b0de",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "montserrat-bold",
+              fontSize: 26,
+              margin: 20,
+              color: "#fff",
+            }}
+          >
+            GoNative
+          </Text>
+        </View>
+      )}
+
+      {selectedCard ? (
+        <Card card={selectedCard} />
+      ) : (
+        <ScrollView>
+          {list.map((card) => (
+            <TouchableOpacity
+              key={card.name}
+              onPress={() => setSelectedCard(card)}
+              activeOpacity={1}
+            >
+              <Card card={card} />
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 }
--- templates/animations/GoNative/list.js
+++ templates/animations/GoNative/list.js
@@ -1,5 +1,19 @@
 const list = [
   {
+    name: "Olympus",
+    description: "Land of gods",
+    image: require("../../../assets/images/sunset.jpg"),
+    likes: 345,
+    color: "#9a75f0",
+  },
+  {
+    name: "Valhalla",
+    description: "Boundless life in Valhalla",
+    image: require("../../../assets/images/paradise.jpg"),
+    likes: 400,
+    color: "#c74bf6",
+  },
+  {
     name: "Neverland",
     description: "The place where you take flight and don't ever land",
     image: require("../../../assets/images/paradise3.jpg"),
@@ -10,29 +24,15 @@ const list = [
     name: "Gryffindor",
     description: "Let the magic rule",
     image: require("../../../assets/images/paradise4.jpeg"),
-    likes: 0,
+    likes: 134,
     color: "#41c8e5",
   },
   {
     name: "Narnia",
     description: "Where the impossible is possible",
     image: require("../../../assets/images/paradise.png"),
-    likes: 0,
+    likes: 279,
     color: "#6e6eeb",
-  },
-  {
-    name: "Olympus",
-    description: "Land of gods",
-    image: require("../../../assets/images/sunset.jpg"),
-    likes: 0,
-    color: "#9a75f0",
-  },
-  {
-    name: "Valhalla",
-    description: "Boundless life in Valhalla",
-    image: require("../../../assets/images/paradise.jpg"),
-    likes: 0,
-    color: "#c74bf6",
   },
 ];
 
