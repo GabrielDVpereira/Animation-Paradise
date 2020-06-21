2ba1db1d488a6819f842e573b823b7f7cfa88e95
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-21 17:44:20 -0300

    Added pinch animation

--- templates/animations/swipe-image-02/index.js
+++ templates/animations/swipe-image-02/index.js
@@ -1,14 +1,70 @@
-import React from "react";
-import { ScrollView, View, Image, Dimensions } from "react-native";
+import React, { useRef, useState } from "react";
+import { Dimensions } from "react-native";
 import images from "../../../assets/images";
-import Animated, { add } from "react-native-reanimated";
+import Animated from "react-native-reanimated";
+import { PinchGestureHandler, State } from "react-native-gesture-handler";
 
 const { width: screen_width, height: screen_height } = Dimensions.get("window");
 const imagePadding = 50;
 const imageWidth = screen_width - imagePadding * 2;
 
+const { add, multiply, sub, interpolate, useCode, call } = Animated;
+
 export default function SwipeImage2() {
-  const offsetx = new Animated.Value(0);
+  const offsetx = useRef(new Animated.Value(0)).current;
+  const imageScaleOnPinch = useRef(new Animated.Value(1)).current;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [pinchActive, setPinchActive] = useState(false);
+
+  useCode(() => {
+    return call([offsetx], (offsetx) => {
+      setCurrentImageIndex(Math.ceil(offsetx[0] / imageWidth));
+    });
+  }, [offsetx]);
+
+  function getImageTransformation(index) {
+    if (pinchActive && currentImageIndex == index) {
+      return {
+        scale: interpolate(imageScaleOnPinch, {
+          inputRange: [0, 1, 2],
+          outputRange: [1, 1, 1.5],
+        }),
+      };
+    }
+    //call a fuction to make a spring animation to the imageScaleOnPinch before its returned
+    return {
+      scale: interpolate(offsetx, {
+        inputRange: [
+          multiply(sub(index, 1), imageWidth),
+          multiply(index, imageWidth),
+          multiply(add(index, 1), imageWidth),
+        ],
+        outputRange: [0.9, 1, 0.9],
+      }),
+    };
+  }
+
+  function getImageOpacity(index) {
+    if (pinchActive && currentImageIndex !== index) return 0; //Apply an timing to bring opacity to 0
+
+    return interpolate(offsetx, {
+      inputRange: [
+        multiply(sub(index, 1), imageWidth),
+        multiply(index, imageWidth),
+        multiply(add(index, 1), imageWidth),
+      ],
+      outputRange: [0.5, 1, 0.5],
+    });
+  }
+
+  function handleStateChanged({ nativeEvent }) {
+    if (nativeEvent.state == State.ACTIVE) {
+      setPinchActive(true);
+    } else {
+      setPinchActive(false);
+    }
+  }
+
   return (
     <Animated.ScrollView
       horizontal
@@ -25,40 +81,31 @@ export default function SwipeImage2() {
       snapToAlignment="right"
       decelerationRate="fast"
       snapToInterval={imageWidth}
-      style={{ paddingVertical: 50 }}
-      contentContainerStyle={{ paddingHorizontal: imagePadding }}
+      contentContainerStyle={{
+        paddingHorizontal: imagePadding,
+        alignItems: "center",
+      }}
     >
       {images.map((image, index) => (
-        <Animated.Image
-          source={image.uri}
-          style={{
-            width: imageWidth,
-            height: 400,
-            backgroundColor: "#000",
-            borderEndWidth: 1,
-            borderRadius: 20,
-            transform: [
-              {
-                scale: Animated.interpolate(offsetx, {
-                  inputRange: [
-                    Animated.multiply(Animated.sub(index, 1), imageWidth),
-                    Animated.multiply(index, imageWidth),
-                    Animated.multiply(Animated.add(index, 1), imageWidth),
-                  ],
-                  outputRange: [0.9, 1, 0.9],
-                }),
-              },
-            ],
-            opacity: Animated.interpolate(offsetx, {
-              inputRange: [
-                Animated.multiply(Animated.sub(index, 1), imageWidth),
-                Animated.multiply(index, imageWidth),
-                Animated.multiply(Animated.add(index, 1), imageWidth),
-              ],
-              outputRange: [0.5, 1, 0.5],
-            }),
-          }}
-        />
+        <PinchGestureHandler
+          onGestureEvent={Animated.event([
+            { nativeEvent: { scale: imageScaleOnPinch } },
+          ])}
+          onHandlerStateChange={handleStateChanged}
+        >
+          <Animated.Image
+            source={image.uri}
+            style={{
+              width: imageWidth,
+              height: 400,
+              backgroundColor: "#fff",
+              borderEndWidth: 1,
+              borderRadius: 20,
+              transform: [getImageTransformation(index)],
+              opacity: getImageOpacity(index),
+            }}
+          />
+        </PinchGestureHandler>
       ))}
     </Animated.ScrollView>
   );
