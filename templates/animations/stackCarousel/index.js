558ad91d22824bc39cdaceba8a1c92c582076613
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-23 23:03:59 -0300

    structure for stack carousel animation

---
+++ templates/animations/stackCarousel/index.js
@@ -0,0 +1,98 @@
+import * as React from "react";
+import {
+  Image,
+  FlatList,
+  Dimensions,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+
+import { EvilIcons } from "@expo/vector-icons";
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from "react-native-gesture-handler";
+
+import { DATA } from "./data";
+
+const { width, height } = Dimensions.get("screen");
+
+const OVERFLOW_HEIGHT = 80;
+const SPACING = 10;
+const ITEM_WIDTH = width * 0.8;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+
+const OverflowItems = ({ data }) => {
+  return (
+    <View style={styles.overflowContainer}>
+      <View>
+        {data.map((item, index) => {
+          return (
+            <View key={index} style={styles.itemContainer}>
+              <Text style={[styles.title]} numberOfLines={1}>
+                {item.title}
+              </Text>
+              <View style={styles.itemContainerRow}>
+                <Text style={[styles.location]}>
+                  <EvilIcons
+                    name="location"
+                    size={16}
+                    color="black"
+                    style={{ marginRight: 5 }}
+                  />
+                  {item.location}
+                </Text>
+                <Text style={[styles.date]}>{item.date}</Text>
+              </View>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+export default function StackCarousel() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <OverflowItems data={DATA} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: -1,
+  },
+  location: {
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 12,
+  },
+  itemContainer: {
+    height: OVERFLOW_HEIGHT,
+    padding: SPACING * 2,
+  },
+  itemContainerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  overflowContainer: {
+    eight: OVERFLOW_HEIGHT,
+    overflow: "hidden",
+  },
+});
